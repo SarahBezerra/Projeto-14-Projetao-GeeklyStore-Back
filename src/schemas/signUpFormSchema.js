746c19e51b44c4481c7ee0e3ebbf68d08e2c61cb
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-02-10 14:04:01 -0300

    Fix: Sign-Up schema

--- src/schemas/signUpFormSchema.js
+++ src/schemas/signUpFormSchema.js
@@ -3,6 +3,7 @@ import joi from "joi";
 const signUpFormSchema = joi.object({
     name: joi.string().pattern(/^[a-záàâãéèêíïóôõöúçñ ,.'-]+$/i).required(),
     email: joi.string().email().required(),
+    avatar: joi.string().uri().required(),
     password: joi.string().required(),
     confirmPassword: joi.string().required()
 })
