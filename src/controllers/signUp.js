527a5cfc313a46bf75d018705778a683c2b41fb0
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-02-10 13:59:27 -0300

    Feat: Sign-Up validation, Router, controller

---
+++ src/controllers/signUp.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt"
+import db from "../db.js"
+
+export default async function signUp (request, response){
+
+    try {
+        const user = request.body;
+        const passwordHashed = bcrypt.hashSync(user.password,10);
+        delete user.confirmPassword;
+
+        const usersCollection = db.collection("users");
+        const userData = await usersCollection.insertOne({...user, email: user.email.toLowerCase(), password: passwordHashed});
+
+        if(!userData){
+            response.sendStatus(401);
+            return;
+        }
+
+        response.sendStatus(200);
+        
+    } catch (error) {
+
+        console.log(error);
+        response.sendStatus(500);
+    }
+}
--- src/schemas/signUpFormSchema.js
+++ src/schemas/signUpFormSchema.js
@@ -4,7 +4,7 @@ const signUpFormSchema = joi.object({
     name: joi.string().pattern(/^[a-záàâãéèêíïóôõöúçñ ,.'-]+$/i).required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
-    passwordConfirmed: joi.string().required()
+    confirmPassword: joi.string().required()
 })
 
 export default signUpFormSchema;
