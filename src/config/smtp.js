49ac97c14aaf2a2f0ed6c08be7487f304d4b825a
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-02-11 20:37:01 -0300

    Feat: send email route

---
+++ src/config/smtp.js
@@ -0,0 +1,8 @@
+const smtp = {
+    host: "smtp.gmail.com",
+    port: 587,
+    user: "geeklystorenoreply@gmail.com",
+    pass: "Geekly/123"
+}
+
+export default smtp;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import CheckoutRouter from "./CheckoutRouter.js";
 import signInRouter from "./signInRouter.js";
 import signUpRouter from "./signUpRouter.js";
+
 
 
 const router = Router();
 
 router.use(signInRouter);
-router.use(signUpRouter)
+router.use(signUpRouter);
+router.use(CheckoutRouter);
 
 export default router;
