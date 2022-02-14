a2a7ea5ba8c4b284123c76f29d4c0218fa5f2da3
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-02-14 18:20:03 -0300

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import checkoutRouter from "./checkoutRouter.js";
 import productsRouter from "./productsRouter.js";
 import signInRouter from "./signInRouter.js";
 import signUpRouter from "./signUpRouter.js";
-import productsRouter from "./productsRouter.js";
 
 const router = Router();
 
